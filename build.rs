a02dba1cf45a6e740505362ab27e964582a32cf7
Alexander Zahdeh <5175914+alexzah@users.noreply.github.com>
2026-02-16 14:49:43 -0500

    Fixes to fluent style

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let style = std::env::var("SLINT_STYLE").unwrap_or_else(|_| "fluent".to_string());
+    let style = std::env::var("SLINT_STYLE").unwrap_or_else(|_| "fluent-dark".to_string());
     let config = slint_build::CompilerConfiguration::new().with_style(style);
     slint_build::compile_with_config("src/roqtune.slint", config).unwrap();
 }
